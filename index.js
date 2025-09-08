b6510a1bc52f02bf9a2fa265653c653ae3694783
Examinati <72595444+Eximinati@users.noreply.github.com>
2025-09-09 00:01:34 +0700

    Camping-site

---
+++ index.js
@@ -0,0 +1,83 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-icon");
+};
+
+var swiper = new Swiper(".categoryswiper", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {   // ✅ lowercase "p"
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        750: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
+});
+
+var swiper = new Swiper(".Selling-Slidder", {
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {   // ✅ lowercase "p"
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        750: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        920: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    }
+});
+const animate = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay: '400'
+})
+animate.reveal(".nav,.heading,.hero-content h2");
+animate.reveal(".backpack-content", { origin: 'left' })
+animate.reveal(".hero-img img,.btn img, .btn, .backpack img,.single-post", { origin: 'bottom' })
+animate.reveal(".category-box, .product-box,.brand-box,.blog-box,.link-box ,.footer-box h3,.footer-box a,.footer-box p", { interval:100 })
